d98f809f0b222d767fac8dce19becebc07203ad6
moquan <moquan@users.noreply.github.com>
2018-07-08 11:16:37 +0100

    Add files via upload

---
+++ make_submission.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# data_prep.py
+import warnings, copy
+warnings.filterwarnings('ignore')
+import pandas as pd
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+# import seaborn as sns
+# sns.set(style="whitegrid", color_codes=True)
+import cPickle
+
+work_dir = '/home/dawna/tts/mw545/DVExp/hh/'
+
+test_ID_file_name=work_dir+"test_ID.dat"
+IDs = cPickle.load(open(test_ID_file_name, 'rb'))
+# IDs = prices_raw['ID']
+
+predict_output_file_name = work_dir+"predict_output.dat"
+predict_output = cPickle.load(open(predict_output_file_name, 'rb'))
